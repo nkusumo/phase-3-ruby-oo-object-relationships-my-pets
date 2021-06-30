e6b8255c9371f4cac79bae5eda8e520f30c1dc38
nkusumo <nis.kusumo@gmail.com>
2021-06-30 17:18:39 -0400

    commit tree 349c7e6f7dfcbdb6a796d1b37372a955f56ce813

--- spec/04_associations_spec.rb
+++ spec/04_associations_spec.rb
@@ -56,8 +56,8 @@ describe "Associations" do
         @owner.buy_cat("Garfield")
 
         expect(@owner.cats[0].name).to eq("Crookshanks")
-        expect(@owner.cats[1].name).to eq("Whiskers")
-        expect(@owner.cats[2].name).to eq("Garfield")
+        expect(@owner.cats[2].name).to eq("Whiskers")
+        expect(@owner.cats[1].name).to eq("Garfield")
       end
     end
 
